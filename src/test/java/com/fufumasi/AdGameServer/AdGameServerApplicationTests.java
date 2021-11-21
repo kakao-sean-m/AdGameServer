5390bbc6ff3aaeaacc839347efee16f1a1ff14c2
Sanghwa Min <secured8372@gmail.com>
2021-11-21 20:41:54 +0900

    Initial commit

---
+++ src/test/java/com/fufumasi/AdGameServer/AdGameServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fufumasi.AdGameServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdGameServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
